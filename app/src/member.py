10acdd1f6658cf5cda45ae2629b1a9c33a76bcfc
Josh Hill <josh.hill189@gmail.com>
2019-05-04 14:08:50 +1200

    Added login/register

--- app/src/member.py
+++ app/src/member.py
@@ -1,9 +1,9 @@
-from app import db
-
+from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class Member(db.Model):
+class Member(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -17,3 +17,8 @@ class Member(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return Member.query.get(int(id))
