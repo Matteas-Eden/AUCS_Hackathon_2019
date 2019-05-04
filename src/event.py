9de735b7366b2ba343866e22122acf6dfc6912ab
Matteas-Eden <mrmatteden@gmail.com>
2019-05-04 12:12:54 +1200

    Added name, time & info to event.py

--- src/event.py
+++ src/event.py
@@ -1,3 +1,6 @@
 class Event:
-    def __init__(self):
+    def __init__(self,name,info,timestamp):
+        self._name = name
+        self._timestamp = timestamp
+        self._info = info
         pass
