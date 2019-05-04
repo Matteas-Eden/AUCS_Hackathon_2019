14eeabd7193868606c58509b64e2ccbc4c459b5e
Matteas-Eden <mrmatteden@gmail.com>
2019-05-04 19:10:18 +1200

    Removed unnecessary improts

--- app/src/group.py
+++ app/src/group.py
@@ -1,8 +1,6 @@
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-import requests
-import json
 
 class Group(db.Model):
     
